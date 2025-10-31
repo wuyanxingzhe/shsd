8660299ae377da435241a1e737ee09815730b7e0
wuyanxingzhe <152257485+wuyanxingzhe@users.noreply.github.com>
2025-10-31 10:57:49 +0800

    Update route.js

--- app/api/projects/[projectId]/llamaFactory/generate/route.js
+++ app/api/projects/[projectId]/llamaFactory/generate/route.js
@@ -26,7 +26,7 @@ export async function POST(request, { params }) {
 
     // 创建 dataset_info.json 配置
     const config = {
-      [`[Triple Harmony Entropy Motion] [${projectId}] Alpaca`]:
+      [`[Triple Harmony Entropy Motion] [${projectId}] Alpaca`]: {
         file_name: 'alpaca.json',
         columns: {
           prompt: 'instruction',
@@ -35,7 +35,7 @@ export async function POST(request, { params }) {
           system: 'system'
         }
       },
-      [`[Triple Harmony Entropy Motion] [${projectId}] ShareGPT`]:
+      [`[Triple Harmony Entropy Motion] [${projectId}] ShareGPT`]: {
         file_name: 'sharegpt.json',
         formatting: 'sharegpt',
         columns: {
@@ -49,7 +49,7 @@ export async function POST(request, { params }) {
           system_tag: 'system'
         }
       },
-      [`[Triple Harmony Entropy Motion] [${projectId}] multilingual-thinking`]:
+      [`[Triple Harmony Entropy Motion] [${projectId}] multilingual-thinking`]: {
         file_name: 'multilingual-thinking.json',
         formatting: 'multilingual-thinking',
         columns: {
